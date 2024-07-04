20e10fc7c41372b016165be21f052977b00d39ef
Nafis <121489130+nafish4@users.noreply.github.com>
2024-07-04 14:29:36 +0600

    Sprint 1
    User and Admin login process
    User Registration
    Email Verification
    Forget Password
    Making Appointment
    Viewing Appointment
    Cancelling Appointment

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Appointment;
+
+class HomeController extends Controller
+{
+    public function redirect(){
+
+        if(Auth::id()){
+            $usertype=Auth()->user()->usertype;
+            if($usertype=='0'){
+
+                return view('user.home');
+                
+            }
+            
+            else{
+                return view('admin.home');
+            }
+        }
+        else{
+            return redirect()->back();
+        }
+    }
+    public function index(){
+        if(Auth::id())
+        {
+            return redirect('home');
+        }
+        else{
+ 
+            return view('user.home');
+        }
+    }
+    public function appointment(Request $request)
+    {
+        $data=new appointment;
+        $data->name=$request->name;
+        $data->dob=$request->dob;
+        $data->email=$request->email;
+        $data->date=$request->date;
+        $data->phone=$request->number;
+        $data->message=$request->message;
+        $data->doctor=$request->doctor;
+        $data->status='In progress';
+
+        if(Auth::id()){
+            $data->user_id = Auth::user()->id;
+        }
+        $data->save();
+
+        return redirect()->back()->with('message','Appointment successful. We will contact with you soon');
+    }
+    public function myappointment()
+    {
+        if(Auth::id())
+        {
+            $userid=Auth::user()->id;
+            $appoint=appointment::where('user_id',$userid)->get();
+            return view('user.my_appointment',compact('appoint'));
+        }
+        else{
+            return redirect()->back();
+        }
+    }
+    public function cancel_appoint($id){
+        $data=appointment::find($id);
+        $data->delete();
+        return redirect()->back();
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\CartController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/',[HomeController::class,'index']);
+Route::get('/home',[HomeController::class,'redirect'])->middleware('auth','verified');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+
+Route::post('/appointment',[HomeController::class,'appointment']);
+
+Route::get('/myappointment',[HomeController::class,'myappointment']);
+
+Route::get('/cancel_appoint/{id}',[HomeController::class,'cancel_appoint']);
+
